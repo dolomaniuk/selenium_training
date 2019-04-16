c98b12c3e09c00ca70cd6635f9e5780f2f9c913a
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2019-04-16 23:50:01 +0300

    добавлены предусловия для тестов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,4 +4,13 @@ from model.contact import Contact
 
 @pytest.mark.skip
 def test_del_contact(app):
+    if app.contact.count() == 0:
+        test_client = Contact(
+                address="Sovetskaya 32",
+                firstname="Dmitry",
+                middlename="Ivanov",
+                lastname="Pupkin",
+                nickname="pupa"
+        )
+        app.contact.add_contact(test_client)
     app.contact.del_first_contact()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,9 +4,13 @@ from model.group import Group
 
 # @pytest.mark.skip(reason="Not working functional")
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test_group"))
     app.group.modify_first_group(Group(name="New group name"))
 
 
 # @pytest.mark.skip(reason="Not working functional")
 def test_modify_group_logo(app):
+    if app.group.count() == 0:
+        app.group.create(Group(logo="test_group"))
     app.group.modify_first_group(Group(logo="New logo"))
