a587a7a4792bf922d986d3a435e1f3d27194bac9
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2019-04-16 23:30:03 +0300

    добавлена проверка авторизации и выхода из авторизации

--- test/test_add_groupe.py
+++ test/test_add_groupe.py
@@ -7,7 +7,6 @@ from model.group import Group
 def test_add_groupe(app):
     app.group.create(Group(name="new_group", logo="new_logo", comment="new_comment"))
 
-
 # @pytest.mark.skip
 def test_add_empty_groupe(app):
     app.group.create(Group(name="", logo="", comment=""))
