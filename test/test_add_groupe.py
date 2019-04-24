52f9d356ee0f5d1db8db8735e8eab27a71165697
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2019-04-24 22:32:50 +0300

    добавлены кеширование и хеширование

--- test/test_add_groupe.py
+++ test/test_add_groupe.py
@@ -9,14 +9,14 @@ def test_add_groupe(app):
     old_groups = app.group.get_group_list()
     group = Group(name="new_group", logo="new_logo", comment="new_comment")
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_add_empty_groupe(app):
     old_groups = app.group.get_group_list()
     group = Group(name="", logo="", comment="")
