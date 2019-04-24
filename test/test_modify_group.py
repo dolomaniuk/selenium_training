8b87774a95e0ca755f77a8aa0b02966669c8b66f
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2019-04-24 23:55:19 +0300

    добавлены рандомайность

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,23 +1,30 @@
+from random import randrange
+
 import pytest
 from model.group import Group
 
 
-@pytest.mark.skip(reason="Not working functional")
+@pytest.mark.skip(reason="Invalid ID.")
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="test_group")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-@pytest.mark.skip(reason="Not working functional")
+@pytest.mark.skip(reason="Invalid ID.")
 def test_modify_group_logo(app):
     if app.group.count() == 0:
         app.group.create(Group(logo="test_group"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(logo="New logo"))
+    index = randrange(len(old_groups))
+    group = Group(name="test_group")
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
